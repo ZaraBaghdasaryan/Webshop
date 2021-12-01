58d764fb299d46d3730fb9a66d072dd5e3ac2820
ItsSteph <steph.tronier.vip@gmail.com>
2021-12-01 15:50:42 +0100

    added readme file with reference to all pages/code i looked at and might have used as inspo to the code i wrote

---
+++ Webshop/Migrations/20211201144636_CreateDB.cs
@@ -0,0 +1,213 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Webshop.Migrations
+{
+    public partial class CreateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    Email = table.Column<string>(maxLength: 150, nullable: false),
+                    Password = table.Column<string>(maxLength: 20, nullable: false),
+                    IsLoggedin = table.Column<bool>(nullable: false),
+                    Address = table.Column<string>(maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false),
+                    Availability = table.Column<int>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false),
+                    OrdersId = table.Column<int>(nullable: true),
+                    Shopping_CartId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TotalPrice = table.Column<decimal>(nullable: false),
+                    CustomerId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shopping_Carts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Price = table.Column<decimal>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shopping_Carts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shopping_Carts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 1, "Space Suits" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 2, "Space Vehicles" });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "Address", "Email", "FirstName", "IsLoggedin", "LastName", "Password" },
+                values: new object[] { 1, "Vägen 11, 447 74 Vägen", "annabanan@gmail.com", "Anna", false, "Johnson", "1234" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Availability", "CategoryId", "OrdersId", "Price", "ProductName", "Shopping_CartId" },
+                values: new object[] { 1, 10, 1, null, 699, "Space Suit 1", null });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Availability", "CategoryId", "OrdersId", "Price", "ProductName", "Shopping_CartId" },
+                values: new object[] { 2, 10, 2, null, 699, "Space Shuttle", null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ProductId",
+                table: "Orders",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_OrdersId",
+                table: "Products",
+                column: "OrdersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Shopping_CartId",
+                table: "Products",
+                column: "Shopping_CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shopping_Carts_ProductId",
+                table: "Shopping_Carts",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Orders_OrdersId",
+                table: "Products",
+                column: "OrdersId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Shopping_Carts_Shopping_CartId",
+                table: "Products",
+                column: "Shopping_CartId",
+                principalTable: "Shopping_Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Customers_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Products_ProductId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shopping_Carts_Products_ProductId",
+                table: "Shopping_Carts");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Shopping_Carts");
+        }
+    }
+}
---
+++ Webshop/README.md
@@ -0,0 +1,12 @@
+This code was inspired and assisted by:
+
+Documentation/Tutorials
+https://docs.microsoft.com/en-us/ef/ef6/saving/ + subfolders
+https://docs.microsoft.com/en-us/ef/ef6/querying/ + subfolders
+https://www.entityframeworktutorial.net/efcore/saving-data-in-connected-scenario-in-ef-core.aspx
+https://entityframework.net/many-to-many-relationship 
+
+Github
+https://github.com/NBIGrupp5/BikeRental
+https://github.com/DonOllario/Projektarbete-E-Commerce - this was the one you said inspired webshop?
+https://github.com/ZaraBaghdasaryan/Bicycle-Rental-Console-Application - Zaras consoleapp also notified via discord
