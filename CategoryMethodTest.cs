7f487133e3d21a293ee8542b2a239644cb6e0b4f
ItsSteph <70627352+ItsSteph@users.noreply.github.com>
2021-12-03 13:45:39 +0100

    Add files via upload

---
+++ CategoryMethodTest.cs
@@ -0,0 +1,22 @@
+﻿using Shouldly;
+using Webshop.Methods;
+using Xunit;
+
+namespace Tests
+{
+    public class CategoryMethodTest
+    {
+        [Fact]
+        public void GetAllProductsTest()
+        {
+
+            //Act
+            var method = new CategoryMethods();
+            var result = method.GetAllCategories();
+
+            //Assert
+            result.ShouldNotBeEmpty();
+        }
+    }
+}
+
